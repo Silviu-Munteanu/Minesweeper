806a47091cf3a8f076f49f4670fe21e27317cf22
Silviu-Munteanu <79187810+Silviu-Munteanu@users.noreply.github.com>
2022-12-07 15:50:45 +0200

    Variable number of bombs + timer

--- main.py
+++ main.py
@@ -2,18 +2,39 @@ import random
 from tkinter import *
 import math
 import numpy as np
+from apscheduler.schedulers.background import BackgroundScheduler
+def format_time(time):
+    hours=time//3600
+    time-=hours * 3600
+    hours=str(hours)
+    if len(hours) == 1:
+        hours = "0" + hours
+    minutes=time//60
+    time -= minutes * 60
+    minutes=str(minutes)
+    if len(minutes) == 1:
+        minutes= "0" + minutes
+    time = str(time)
+    if len(time) == 1:
+        time= "0" + time
+    return hours + ":" + minutes + ":" + time
+def elapse_second():
+    global time
+    time+=1
+    timer = Label(root,text=format_time(time), font=("Arial", 20)).place(x=1300, y=400)
 def clearFrame(frame):
     for widget in frame.winfo_children():
         widget.destroy()
-global in_menu,in_game,n,m,rectangle_center,cells,first_click,no_bombs,bombs,marked
+global in_menu,in_game,n,m,rectangle_center,cells,first_click,no_bombs,bombs,marked,time
 def generate_random(i,j):
     available=[t for t in range(n*m) if t!=i*m+j]
     bombs=[]
     for i in range(no_bombs):
-        r_index=random.randint(0,len(available)-1)
-        next_bomb=available[r_index]
-        available.remove(available[r_index])
-        bombs.append((next_bomb//m,next_bomb%m))
+        if len(available):
+            r_index=random.randint(0,len(available)-1)
+            next_bomb=available[r_index]
+            available.remove(available[r_index])
+            bombs.append((next_bomb//m,next_bomb%m))
     return bombs
 def check_neighbors(i,j):
     global bombs
@@ -39,7 +60,7 @@ def mark(event):
                 if marked[i,j] == 1:
                     cells[i][j] = Button(root, background='yellow').place(relheight=1 / n, relwidth=0.8 / m, relx=j * 0.8 / m,rely=i * 1 / n)
                 else:
-                    cells[i][j] = Button(root, background='white').place(relheight=1 / n, relwidth=0.8 / m,relx=j * 0.8 / m, rely=i * 1 / n)
+                    cells[i][j] = Button(root).place(relheight=1 / n, relwidth=0.8 / m,relx=j * 0.8 / m, rely=i * 1 / n)
 def reveal_cell(i,j):
     if bombs[i,j] == 1:
         cells[i][j] = Button(root, background='red').place(relheight=1 / n, relwidth=0.8 / m, relx=j * 0.8 / m,rely=i * 1 / n)
@@ -61,7 +82,7 @@ def reveal(event):
                 first_click=1
                 cells[i][j]=Button(root,text=str(check_neighbors(i,j))).place(relheight=1/n,relwidth=0.8/m,relx=j*0.8/m,rely=i*1/n)
                 revealed[i,j]=1
-            else:
+            elif marked[i,j] == 0:
                 if bombs[i,j] == 1:
                     print("Game over!")
                     cells[i][j] = Button(root,background='red').place(relheight=1 / n, relwidth=0.8 / m,relx=j * 0.8 / m, rely=i * 1 / n)
@@ -75,6 +96,11 @@ def reveal(event):
                         revealed[i, j] = 1
 def buildGame(n,m):
     rectangle_center=[]
+    global time
+    time=0
+    scheduler = BackgroundScheduler()
+    scheduler.add_job(elapse_second, 'interval', seconds=1)
+    scheduler.start()
     global in_menu,in_game,cells,marked,revealed
     marked=np.zeros((n,m))
     revealed=np.zeros((n,m))
@@ -88,34 +114,42 @@ def buildGame(n,m):
        cells.append([])
        for j in range(m):
            cells[i].append(Button(root).place(relheight=1/n,relwidth=0.8/m,relx=j*0.8/m,rely=i*1/n))
+    timer = Label(root,text=format_time(time), font=("Arial", 20)).place(x=1300, y=400)
 def buildMenu():
-    global in_menu,in_game,n,m
+    global in_menu,in_game,n,m,no_bombs
     in_menu=1
     in_game=0
     clearFrame(root)
     n = StringVar()
     m = StringVar()
+    no_bombs = StringVar()
     n_text = Label(root, text="No. rows:", font=("Arial", 30)).place(x=500, y=200)
     n_entry = Entry(root, textvariable=n, font=("Arial", 30)).place(x=700, y=200)
     m_text = Label(root, text="No. columns:", font=("Arial", 30)).place(x=450, y=400)
     m_entry = Entry(root, textvariable=m, font=("Arial", 30)).place(x=700, y=400)
+    b_text = Label(root, text="No. bombs:", font=("Arial", 30)).place(x=470, y=600)
+    b_entry = Entry(root, textvariable=no_bombs, font=("Arial", 30)).place(x=700, y=600)
     start_button = Button(root, command=lambda: play(n, m), text="Play", font=("Arial", 30))
-    start_button.place(x=700, y=500)
+    start_button.place(x=700, y=700)
     root.mainloop()
 def play(n_text,m_text):
-    global n,m
+    global n,m,no_bombs
     n=n_text.get()
     m=m_text.get()
+    no_bombs=no_bombs.get()
     for i in n:
         if i.isdigit() == False:
             return False
     for i in m:
         if i.isdigit() == False:
             return False
+    for i in no_bombs:
+        if i.isdigit() == False:
+            return False
     n=int(n)
     m=int(m)
+    no_bombs=int(no_bombs)
     buildGame(n,m)
-no_bombs=2
 first_click=0
 root = Tk()
 root.title("Minesweeper")
