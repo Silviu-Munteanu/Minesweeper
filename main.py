079e4d908eecf8cf7b126f0b25e6126fb611c6dc
Silviu-Munteanu <79187810+Silviu-Munteanu@users.noreply.github.com>
2022-12-07 10:54:11 +0200

    revealed bombs are red and reveal everything + mark for bombs

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ import numpy as np
 def clearFrame(frame):
     for widget in frame.winfo_children():
         widget.destroy()
-global in_menu,in_game,n,m,rectangle_center,cells,first_click,no_bombs,bombs
+global in_menu,in_game,n,m,rectangle_center,cells,first_click,no_bombs,bombs,marked
 def generate_random(i,j):
     available=[t for t in range(n*m) if t!=i*m+j]
     bombs=[]
@@ -21,36 +21,63 @@ def check_neighbors(i,j):
     print(bombs)
     for k in range(-1,2,1):
         for p in range(-1,2,1):
-            print(i+k,j+p)
-            if i+k<n and j+p<m:
+            if i+k<n and j+p<m and i+k>=0 and j+p>=0:
                 if bombs[i+k,j+p] == 1 and (k!=0 or p!=0):
+                    print(i,j,":",i+k,j+p)
                     no_n+=1
     return no_n
+def mark(event):
+    global marked
+    if in_game == 1:
+        abs_x = root.winfo_pointerx() - root.winfo_rootx()
+        abs_y = root.winfo_pointery() - root.winfo_rooty()
+        if abs_y < 800 and abs_x < 0.8 * 1600:
+            i = int(abs_y / 800 * n)
+            j = int(abs_x / (0.8 * 1600) * m)
+            if revealed[i,j] == 0:
+                marked[i,j]=1-marked[i,j]
+                if marked[i,j] == 1:
+                    cells[i][j] = Button(root, background='yellow').place(relheight=1 / n, relwidth=0.8 / m, relx=j * 0.8 / m,rely=i * 1 / n)
+                else:
+                    cells[i][j] = Button(root, background='white').place(relheight=1 / n, relwidth=0.8 / m,relx=j * 0.8 / m, rely=i * 1 / n)
+def reveal_cell(i,j):
+    if bombs[i,j] == 1:
+        cells[i][j] = Button(root, background='red').place(relheight=1 / n, relwidth=0.8 / m, relx=j * 0.8 / m,rely=i * 1 / n)
+    else:
+        cells[i][j] = Button(root, text=str(check_neighbors(i,j))).place(relheight=1 / n, relwidth=0.8 / m, relx=j * 0.8 / m,rely=i * 1 / n)
 def reveal(event):
-    global n,m,rectangle_center,cells,first_click,bombs
+    global n,m,rectangle_center,cells,first_click,bombs,marked,revealed
     if in_game==1:
         abs_x= root.winfo_pointerx() - root.winfo_rootx()
         abs_y= root.winfo_pointery() - root.winfo_rooty()
         if abs_y <800 and abs_x<0.8 * 1600:
             i=int(abs_y/800*n)
             j=int(abs_x/(0.8*1600)*m)
-            if first_click!=1:
+            if first_click!=1 and marked[i,j] == 0:
                 index_bombs=generate_random(i,j)
                 bombs=np.zeros((n,m))
                 for k in index_bombs:
                     bombs[k[0],k[1]]=1
                 first_click=1
-            if bombs[i,j] == 1:
-                print("Game over!")
-          #  for i in range(n):
-               # for j in range(m):
-                   # reveal_cell()
-            n_neighbors=check_neighbors(i,j)
-            print(n_neighbors)
-            cells[i][j]=Button(root,text=str(n_neighbors)).place(relheight=1/n,relwidth=0.8/m,relx=j*0.8/m,rely=i*1/n)
+                cells[i][j]=Button(root,text=str(check_neighbors(i,j))).place(relheight=1/n,relwidth=0.8/m,relx=j*0.8/m,rely=i*1/n)
+                revealed[i,j]=1
+            else:
+                if bombs[i,j] == 1:
+                    print("Game over!")
+                    cells[i][j] = Button(root,background='red').place(relheight=1 / n, relwidth=0.8 / m,relx=j * 0.8 / m, rely=i * 1 / n)
+                    revealed[i, j] = 1
+                    for i in range(n):
+                        for j in range(m):
+                            reveal_cell(i,j)
+                else:
+                    if marked[i,j] == 0:
+                        cells[i][j]=Button(root,text=str(check_neighbors(i,j))).place(relheight=1/n,relwidth=0.8/m,relx=j*0.8/m,rely=i*1/n)
+                        revealed[i, j] = 1
 def buildGame(n,m):
     rectangle_center=[]
-    global in_menu,in_game,cells
+    global in_menu,in_game,cells,marked,revealed
+    marked=np.zeros((n,m))
+    revealed=np.zeros((n,m))
     in_menu=0
     in_game=1
     clearFrame(root)
@@ -94,4 +121,5 @@ root = Tk()
 root.title("Minesweeper")
 root.geometry("1600x800")
 root.bind("<Button-1>",reveal)
+root.bind("<Button-3>",mark)
 buildMenu()
